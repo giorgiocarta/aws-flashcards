e72f237969273a63b508d62461cb17ad824c925f
Giorgio Carta <giorgio.carta@distilled.ie>
2020-04-01 17:22:20 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
 	name='aws-flashcards',
-	version='0.0.1',
+	version='0.0.2',
 	packages=[''],
 	url='https://github.com/donedeal-giorgio/aws-flashcards.git',
 	license='MIT',
